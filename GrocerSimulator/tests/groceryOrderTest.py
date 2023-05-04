5aed44ece65f9f846e02b9f32e310ac24b1f5a59
Keenan-Faure <keenan.asterisk@gmail.com>
2023-05-04 16:58:30 +0200

    update

--- GrocerSimulator/tests/groceryOrderTest.py
+++ GrocerSimulator/tests/groceryOrderTest.py
@@ -27,50 +27,47 @@ class GroceryListTest(unittest.TestCase):
         payment = None
         if(cash == False):
             bank = Bank("BankName", 45212, "0215478963")
-            payment = BankPayment(203.5, bank)
+            payment = BankPayment(20003.5, bank)
         else:
-            payment = CashPayment(203.5, "firstName lastName")
+            payment = CashPayment(20003.5, "firstName lastName")
         customer = Customer("firstName", "lastName", address, payment)
 
         #products
-        product_one = prod("GenImp-V-AA", "Ballad in Goblets - Venti", 5, 1500.0, "Stock2Shop")
-        product_two = prod("GenImp-B-HC", "Barbara", 5, 1000.0, "Internal")
-        product_three = prod("GenImp-SK-HC", "Sango", 5, 1699.0, "Internal")
-        product_four = prod("GenImp-C-EP", "Cyno", 5, 1350.5, "Internal")
+        product_one = prod("GenImp-V-AA", "Ballad in Goblets - Venti", 2, 1500.0, "Stock2Shop")
+        product_two = prod("GenImp-C-EP", "Cyno", 1, 1350.5, "Internal")
         if(reset == True):
             groceryList = GroceryList()
             groceryList.reset_list()
             groceryList.add_product(product_one)
             groceryList.add_product(product_two)
-            groceryList.add_product(product_three)
-            groceryList.add_product(product_four)
         else:
             groceryList = GroceryList()
             groceryList.add_product(product_one)
             groceryList.add_product(product_two)
-            groceryList.add_product(product_three)
-            groceryList.add_product(product_four)
 
         return GroceryOrder(customer, groceryList)
     
     def test_constructor(self):
         groceryOrder = GroceryListTest.init_class(True)
         self.assertEqual(groceryOrder.get_customer().get_address().get_address1(), "1st street")
-        self.assertEqual(groceryOrder.get_customer().get_payment_method().get_amount(), 203.5)
-        self.assertEqual(len(groceryOrder.get_grocery_list().get_grocery_list()), 4)
+        self.assertEqual(groceryOrder.get_customer().get_payment_method().get_amount(), 20003.5)
+        self.assertEqual(len(groceryOrder.get_grocery_list().get_grocery_list()), 2)
 
         groceryOrder = GroceryListTest.init_class()
         self.assertEqual(groceryOrder.get_customer().get_address().get_address1(), "1st street")
-        self.assertEqual(groceryOrder.get_customer().get_payment_method().get_amount(), 203.5)
-        self.assertEqual(len(groceryOrder.get_grocery_list().get_grocery_list()), 4)
+        self.assertEqual(groceryOrder.get_customer().get_payment_method().get_amount(), 20003.5)
+        self.assertEqual(len(groceryOrder.get_grocery_list().get_grocery_list()), 2)
     
     def test_order_confirm(self):
         groceryOrder = GroceryListTest.init_class()
-        list = [prod("GenImp-C-EP", "Cyno", 1, 1350.5, "Internal")]
+        list = [
+            prod("GenImp-C-EP", "Cyno", 10, 1350.5, "Internal"),
+            prod("GenImp-V-AA", "Ballad in Goblets - Venti", 5, 1500.0, "Stock2Shop")
+        ]
         
         result = groceryOrder.orderConfirm(list)
-        self.assertEqual([], result[0])
-        self.assertEqual(0, result[1])
+        self.assertEqual(2, len(result[0]))
+        self.assertEqual(4350, result[1])
         
 if __name__ == "__main__":
     unittest.main()

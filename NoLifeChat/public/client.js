441722a4ae287ae2c53c0a97bdf1f01f05245566
Keenan-Faure <keenan.asterisk@gmail.com>
2023-05-22 21:03:17 +0200

    update

--- NoLifeChat/public/client.js
+++ NoLifeChat/public/client.js
@@ -49,7 +49,7 @@ async function query_url(url)
     let query_params = urlObj.search;
     if(query_params != "")
     {
-        query_params = query_params.slice(1, -1);
+        query_params = query_params.slice(1, query_params.length);
         let queries = query_params.split("&");
         if(queries.length > 0)
         {
@@ -58,24 +58,40 @@ async function query_url(url)
                 let param = queries[i].split("=");
                 if(param[0] == "user")
                 {
-                    console.log(await get_file());
-                    const keys = Object.keys();
-                    if(param[0] in keys)
+                    let file = await get_file();
+                    const keys = Object.keys(file);
+                    if(keys.includes(param[1]))
                     {
-                        console.log("yep");
-                        return true;
+                        password = (prompt("Please enter your password:"));
+                        return (password == file[param[1]]) ? true : "incorrect password";
                     }
+                    return "incorrect password";
                 }
-                return "user param not specified";
+                return "incorrect param key, expected 'user'";
             }
         }
-        return "No query params found";
+        return "no query params";
     }
-    return "No query params found";
+    return "http://localhost:{{port}}/?{{user}}={{password}}"
 }
 
 async function get_file()
 {
     let response = await fetch('./users.json');
     return response.json();
+}
+
+/**
+ * 
+ * @param {String} message 
+ */
+function remove_dom(message)
+{
+    document.querySelector('.container').remove();
+    element = document.createElement('a');
+    element.style.fontSize = "22px";
+    element.href = window.location.href;
+    if(message){element.innerHTML = message; }
+    else{ element.innerHTML = 'Incorrect password, please refresh page'; }
+    document.body.appendChild(element);
 }
